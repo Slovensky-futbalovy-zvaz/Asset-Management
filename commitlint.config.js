d129bab3e19d11c61daed3a0407e4fea5c78971d
Ján Letko <jan.letko@icloud.com>
2026-05-18 16:18:57 +0200

    ci: raise commitlint body-max-line-length from 100 to 200
    Default body-max-line-length z @commitlint/config-conventional je 100
    znakov, čo láme commitlint job na dependabot PR-iek. Dependabot generuje
    commit body s release notes URL-ami ktoré bývajú dlhšie:
    
      Bumps [eslint-config-prettier](https://github.com/prettier/eslint-config-prettier) from 9.1.2 to 10.1.8.
    
    To má 112 znakov a fail-uje na body-max-line-length pravidlo.
    
    200 znakov je dosť veľký priestor pre ľudské commit messages aby
    nepokrýval prose paste-y, ale akceptuje strojovo generované URL riadky
    z dependabot/renovate/CI bot-ov. Tým unblockne dependabot PR-iek bez
    toho aby sme stratili guard proti omylom dlhým paragraphom v body.

--- commitlint.config.js
+++ commitlint.config.js
@@ -27,5 +27,11 @@ export default {
     'header-max-length': [2, 'always', 100],
     'body-leading-blank': [2, 'always'],
     'footer-leading-blank': [2, 'always'],
+    // Zvýšené z default 100 na 200 lebo dependabot generuje commit body
+    // s release notes URL-ami ktoré bývajú dlhšie ako 100 znakov,
+    // napr.: "Bumps [eslint-config-prettier](https://github.com/prettier/...".
+    // 200 znakov je dost na bežné človekom písané body riadky
+    // a zároveň akceptuje strojovo generované dependabot URL paste.
+    'body-max-line-length': [2, 'always', 200],
   },
 };
